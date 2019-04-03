c866974405a1b4528f5ad027ef6bc1c38b85f24c
wheatandcat <wheatandcat@gmail.com>
2019-04-03 23:47:43 +0900

    add html

--- src/App.js
+++ src/App.js
@@ -1,28 +1,37 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { withStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Top from "./components/pages/Top";
+import icon from "./images/icon.png";
+
+const styles = {
+  root: {
+    flexGrow: 1,
+    height: "100vh"
+  }
+};
 
 class App extends Component {
   render() {
+    const { classes } = this.props;
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className={classes.root}>
+        <AppBar position="static" color="default">
+          <Toolbar>
+            <Typography variant="h5" color="inherit">
+              <img src={icon} height="50" alt="icon" />
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        <div style={{ backgroundColor: "#28AEB2", height: "100%" }}>
+          <Top />
+        </div>
       </div>
     );
   }
 }
 
-export default App;
+export default withStyles(styles)(App);
---
+++ src/components/pages/Top.jsx
@@ -0,0 +1,97 @@
+import React, { Component } from "react";
+import Grid from "@material-ui/core/Grid";
+import iphone from "../../images/iphone.png";
+import googlePlayBadge from "../../images/google-play-badge.png";
+import appleStore from "../../images/apple-store.svg";
+
+export default class extends Component {
+  render() {
+    return (
+      <>
+        <Grid container spacing={16}>
+          <Grid
+            item
+            sm={6}
+            xs={12}
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              height: "350px",
+              backgroundColor: "#28AEB2"
+              //paddingLeft: "20rem"
+            }}
+          >
+            <div style={{ color: "#fff", fontSize: "45px", fontWeight: "600" }}>
+              ペペロミア
+            </div>
+          </Grid>
+          <Grid
+            item
+            sm={6}
+            xs={12}
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              backgroundColor: "#28AEB2"
+              //paddingRight: "20rem"
+            }}
+          >
+            <img src={iphone} height="300" alt="iphone" />
+          </Grid>
+          <Grid
+            item
+            xs={12}
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              height: "200px",
+              padding: "50px",
+              backgroundColor: "#fff"
+            }}
+          >
+            <div style={{ color: "#000", fontSize: "16px", fontWeight: "600" }}>
+              予定を簡単登録、共有できる！
+            </div>
+          </Grid>
+          <Grid
+            item
+            xs={12}
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              height: "200px",
+              padding: "50px",
+              backgroundColor: "#28AEB2"
+            }}
+          >
+            <img src={appleStore} height="50" alt="appleStore" />
+            <img src={googlePlayBadge} height="70" alt="googlePlayBadge" />
+          </Grid>
+        </Grid>
+        <div
+          item
+          xs={12}
+          style={{
+            display: "flex",
+            justifyContent: "flex-end	",
+            alignItems: "flex-end",
+            height: "100px",
+            padding: "10px",
+            backgroundColor: "#232F3E"
+          }}
+        >
+          <div style={{ color: "#fff", fontSize: "13px", padding: "1rem" }}>
+            利用規約
+          </div>
+          <div style={{ color: "#fff", fontSize: "13px", padding: "1rem" }}>
+            プライパシーポリシー
+          </div>
+        </div>
+      </>
+    );
+  }
+}
