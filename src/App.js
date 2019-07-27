303a8b9419c35f44324220339d8beb3186b7baf7
wheatandcat <wheatandcat@gmail.com>
2019-07-27 15:01:11 +0900

    renewal

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import { withStyles } from "@material-ui/core/styles";
-import AppBar from "@material-ui/core/AppBar";
-import Toolbar from "@material-ui/core/Toolbar";
-import Typography from "@material-ui/core/Typography";
+import { withStyles, createMuiTheme } from "@material-ui/core/styles";
+import { ThemeProvider as MaterialUIThemeProvider } from "@material-ui/styles";
+import styled, { ThemeProvider } from "styled-components";
 import IconButton from "@material-ui/core/IconButton";
 import Top from "./components/pages/Top";
 import Tos from "./components/pages/Tos";
 import Policy from "./components/pages/Policy";
 import icon from "./images/icon.png";
 import github from "./images/github.png";
+import theme from "./theme";
+
+const uiTheme = createMuiTheme({
+  palette: {
+    primary: {
+      main: theme.color.lightGreen
+    },
+    secondary: {
+      main: theme.color.lightBlue
+    },
+    default: {
+      main: theme.color.main
+    }
+  }
+});
 
 const styles = {
   root: {
@@ -24,34 +38,44 @@ class App extends Component {
 
     return (
       <Router>
-        <div className={classes.root}>
-          <AppBar position="static" color="default" style={{ height: "70px" }}>
-            <Toolbar>
-              <Typography variant="h5" color="inherit" style={{ flexGrow: 1 }}>
-                <img src={icon} height="50" alt="icon" />
-              </Typography>
-              <IconButton
-                href="https://github.com/wheatandcat/Peperomia"
-                target="_blank"
-              >
-                <img
-                  src={github}
-                  height="28"
-                  alt="github"
-                  style={{ right: 0 }}
-                />
-              </IconButton>
-            </Toolbar>
-          </AppBar>
-          <div style={{ backgroundColor: "#28AEB2", height: "100%" }}>
-            <Route path="/" exact component={Top} />
-            <Route path="/tos" exact component={Tos} />
-            <Route path="/policy" exact component={Policy} />
-          </div>
-        </div>
+        <MaterialUIThemeProvider theme={uiTheme}>
+          <ThemeProvider theme={theme}>
+            <div className={classes.root}>
+              <AppBar position="static" color="default">
+                <IconButton href="https://peperomia.app">
+                  <img src={icon} height="30" alt="icon" />
+                </IconButton>
+                <IconButton
+                  href="https://github.com/wheatandcat/Peperomia"
+                  target="_blank"
+                  style={{}}
+                >
+                  <img
+                    src={github}
+                    height="25"
+                    alt="github"
+                    style={{ right: 0 }}
+                  />
+                </IconButton>
+              </AppBar>
+              <div style={{ backgroundColor: "#28AEB2", height: "100%" }}>
+                <Route path="/" exact component={Top} />
+                <Route path="/tos" exact component={Tos} />
+                <Route path="/policy" exact component={Policy} />
+              </div>
+            </div>
+          </ThemeProvider>
+        </MaterialUIThemeProvider>
       </Router>
     );
   }
 }
+
+const AppBar = styled.div`
+  display: flex;
+  justify-content: space-between;
+  background-color: ${props => props.theme.color.main};
+  height: 55px;
+`;
 
 export default withStyles(styles)(App);
---
+++ src/theme.js
@@ -0,0 +1,25 @@
+const theme = {
+  color: {
+    beige: "#E4E4C8",
+    darkGray: "#4F4F4F",
+    didgerBlue: "#A1CBF3",
+    gray: "#828282",
+    lightBlue: "#A7EFFF",
+    lightEmerald: "#98D181",
+    lightGray: "#D3D3D3",
+    lightGreen: "#ADCF01",
+    lightNavy: "#CECEE6",
+    lightOrange: "#FFE3AC",
+    lightPink: "#F1DBF9",
+    lightRed: "#FF8C8C",
+    lightYellow: "#FBFDB0",
+    highLightGray: "#F2F2F2",
+    white: "#fff",
+    pink: "#FCCBCB",
+    main: "#006835",
+    red: "#E15757",
+    sky: "#00C2ED"
+  }
+};
+
+export default theme;
