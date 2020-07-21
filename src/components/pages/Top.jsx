a28b2792a32498b686d84695df9043878947d0e1
wheatandcat <wheatandcat@gmail.com>
2020-07-21 23:31:20 +0900

    separate component

--- src/components/pages/Top.jsx
+++ src/components/pages/Top.jsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
+import { animated, useSpring } from 'react-spring';
 import Doujinshi from '../organisms/Doujinshi';
 import AppTitle from '../organisms/AppTitle';
 import AppInfo1 from '../organisms/AppInfo1';
@@ -7,10 +8,32 @@ import GitHub from '../organisms/GitHub';
 import Blog from '../organisms/Blog';
 import AppFooter from '../organisms/AppFooter';
 
+const calc = o => `translateY(${o * 0.1}px)`;
+
 export default () => {
+  const ref = useRef();
+  const [{ offset }, set] = useSpring(() => ({ offset: 0 }));
+
+  const handleScroll = () => {
+    const posY = ref.current.getBoundingClientRect().top;
+    const offset = window.pageYOffset - posY;
+    set({ offset });
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  });
+
+
   return (
-    <>
-      <AppTitle />
+    <div ref={ref}>
+
+        <AppTitle />
+  
       <Doujinshi />
       <AppInfo1 />
       <AppInfo2 />
@@ -18,6 +41,6 @@ export default () => {
       <Blog />
 
       <AppFooter />
-    </>
+    </div>
   );
 };
